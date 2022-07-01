28724f7db906ab3964b6120579d3642ad422807a
dmitryagoshkov <agodimon@mail.ru>
2022-07-01 20:11:10 +0500

    подключил библиотеку кирила розова viewBindingDelegate с рефлексией

--- app/src/main/java/com/example/movingcar/MainActivity.kt
+++ app/src/main/java/com/example/movingcar/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.movingcar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.movingcar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewBinding: ActivityMainBinding by viewBinding(CreateMethod.INFLATE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
     }
 }
