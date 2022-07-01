970af803ab336b7188a0d8cbc1540f396eb7da86
dmitryagoshkov <agodimon@mail.ru>
2022-07-01 22:06:39 +0500

    добавил класс CarAnimation.kt картинки анимацию Анимацию View с интерполятором OvershootInterpolator

--- app/src/main/java/com/example/movingcar/MainActivity.kt
+++ app/src/main/java/com/example/movingcar/MainActivity.kt
@@ -2,14 +2,37 @@ package com.example.movingcar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.*
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.movingcar.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     private val viewBinding: ActivityMainBinding by viewBinding(CreateMethod.INFLATE)
+    private var isMoved: Boolean = true
+    private val anim: CarAnimation by lazy {
+        CarAnimation(
+            maxYOfScreen = viewBinding.root.height.toFloat(),
+            maxXOfScreen = viewBinding.root.width.toFloat(),
+            view = viewBinding.speedCar,
+            interpolator = OvershootInterpolator()
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+        initView()
+    }
+    private fun initView() {
+        viewBinding.speedCar.setOnClickListener {
+            onStartAnimation()
+        }
+    }
+
+
+    private fun onStartAnimation() {
+        isMoved = !isMoved
+        anim.onStartAnimation(650, 0f, isMoved)
     }
 }
